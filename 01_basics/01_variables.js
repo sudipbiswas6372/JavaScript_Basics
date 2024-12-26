1e5e13201ce5a506d7e14a43167bcad6ec7768e9
sudipbiswas6372 <135207563+sudipbiswas6372@users.noreply.github.com>
2024-12-26 05:18:05 +0000

    variables and const done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=12
+let accountName="sudip"
+var accountPassword="321"
+//accountId=3
+accountName="harish"
+accountPassword="111"
+/*
+Var-Prefer not to use var.
+beacause of issue in block scope and functional scope
+*/
+console.table([accountName,accountPassword])
