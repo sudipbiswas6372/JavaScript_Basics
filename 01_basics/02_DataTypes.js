5970387b7c8b6bb89184bbde5d96eedb29b2ef52
sudipbiswas6372 <135207563+sudipbiswas6372@users.noreply.github.com>
2024-12-26 06:57:53 +0000

    DataTypes done

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,15 @@
+"use strict"// Treat all js code as newer version
+
+let name="Sudip"
+let age=15
+let isLoggedIn=true
+let state=null
+//number =>2 to power 53
+//bigint
+//string=""
+//boolean=true/false
+//null=standalone value
+//undefine=value not assine
+//symble=unique
+//object
+console.log(typeof null)
